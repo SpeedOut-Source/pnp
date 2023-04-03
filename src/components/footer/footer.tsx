103b044d893144d47c3d5cb0e091b7561f6a1603
Biplob Sutradhar <biplobsd11@gmail.com>
2023-04-03 15:07:34 +0600

    footer divider

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
     <footer className="z-50">
       <div className="mx-5 mt-auto flex items-center justify-center">
         <div className="container">
+          <div className="divider" />
           <div className="mb-4 mt-2 flex flex-col items-center justify-between space-y-2 sm:flex-row sm:space-y-0">
             <span className="text-sm "> © 2023 Biplob Kumar Sutradhar </span>
             <ul className="flex flex-wrap space-x-4 text-sm ">
