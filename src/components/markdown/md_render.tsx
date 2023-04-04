4b6882d939020b62f01f4c708813f514cc6d6cbf
Biplob Sutradhar <biplobsd11@gmail.com>
2023-04-05 01:06:57 +0600

    md adjust

--- src/components/markdown/md_render.tsx
+++ src/components/markdown/md_render.tsx
@@ -3,19 +3,12 @@
 import styles from "../../styles/markdown.module.css";
 import dynamic from "next/dynamic";
 import { type ReactMarkdownOptions } from "react-markdown/lib/react-markdown";
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
 
 const ReactMarkdown: ReactMarkdownOptions | any = dynamic(() =>
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   import("react-markdown").then((e: any) => e.default)
-);
-const rehypeRaw = dynamic(() =>
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  import("rehype-raw").then((e: any) => e.default)
-);
-
-const remarkGfm = dynamic(() =>
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  import("remark-gfm").then((e: any) => e.default)
 );
 
 interface MDRender {
