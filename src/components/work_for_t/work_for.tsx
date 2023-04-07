ab9d9a7f31f505f6a099db862ac640d5a7f75e7c
biplobsd <biplobsd11@gmail.com>
2023-04-07 11:32:01 +0600

    tooltip

--- src/components/work_for_t/work_for.tsx
+++ src/components/work_for_t/work_for.tsx
@@ -10,24 +10,21 @@ export default function WorkFor(props: WorkForProps) {
       </p>
       <div className="mx-auto grid w-full justify-center gap-2 py-4 sm:grid-cols-2 md:mx-2 md:grid-cols-3 md:gap-4">
         {props.company.map((x) => (
-          <Link
-            target="_blank"
-            key={x.name}
-            href={x.homePage}
-            className="w-full"
-          >
-            <div className="p-card flex h-fit w-full cursor-pointer flex-col  py-2">
-              <div className="relative mx-auto mb-2 h-10 w-10 overflow-hidden">
-                <Image
-                  alt={x.name}
-                  src={x.logoUrl}
-                  objectFit="fill"
-                  layout="fill"
-                />
+          <span className="tooltip" data-tip={"Visit website"} key={x.name}>
+            <Link target="_blank" href={x.homePage} className="w-full">
+              <div className="p-card flex h-fit w-full cursor-pointer flex-col  py-2">
+                <div className="relative mx-auto mb-2 h-10 w-10 overflow-hidden">
+                  <Image
+                    alt={x.name}
+                    src={x.logoUrl}
+                    objectFit="fill"
+                    layout="fill"
+                  />
+                </div>
+                <span className="normal-case ">{x.name}</span>
               </div>
-              <span className="normal-case ">{x.name}</span>
-            </div>
-          </Link>
+            </Link>
+          </span>
         ))}
       </div>
     </div>
