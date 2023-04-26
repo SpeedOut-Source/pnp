f26968f946a9bf74a3fe022b6e9272f1edefb084
Biplob Sutradhar <biplobsd11@gmail.com>
2023-04-26 23:21:00 +0600

    verify meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
-      <Head />
+      <Head>
+        <meta name="msvalidate.01" content="2B3B56800802BE35A75AE72C50119295" />
+      </Head>
       <body>
         <Main />
         <NextScript />
