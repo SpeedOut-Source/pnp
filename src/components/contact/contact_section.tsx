5b77198139806b33932b28d61e1731ea08950bf4
Biplob Sutradhar <biplobsd11@gmail.com>
2023-05-14 13:29:55 +0600

    contract section animation disable

--- src/components/contact/contact_section.tsx
+++ src/components/contact/contact_section.tsx
@@ -6,7 +6,7 @@ export default function ContactSection() {
     <div className="mx-auto h-fit max-w-6xl px-4 pt-5 sm:pl-4 sm:pr-0">
       <p className="text-center text-2xl normal-case text-slate-400">Contact</p>
       <div className="flex w-full justify-center">
-        <div className="p-card my-3 h-fit w-fit overflow-visible hover:bg-base-200">
+        <div className="p-card my-3 h-fit w-fit !animate-none overflow-visible hover:bg-base-200 active:!scale-100">
           <div className="mx-2 my-2 flex w-fit justify-center">
             <div className="grid w-fit grid-cols-1 gap-x-2 lg:flex">
               {CONNECT_OPTIONS.map((x) => (
