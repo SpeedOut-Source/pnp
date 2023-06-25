82093fda9ce75e7d63c82bd378d8b77a7a08ef79
Biplob Sutradhar <biplobsd11@gmail.com>
2023-06-25 20:53:08 +0600

    background reading progress

--- src/app_function/hooks/useReadingProgressbar.ts
+++ src/app_function/hooks/useReadingProgressbar.ts
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
- 
+import { useRouter } from "next/router";
+
 /**
  * React Hook to get the scroll percentage from the page, returns value from 0 to 100
  */
 export function useReadingProgress() {
   const [completion, setCompletion] = useState(0);
+  const router = useRouter();
+
   useEffect(() => {
     function updateScrollCompletion() {
       // see how much we have scrolled
@@ -12,18 +15,24 @@ export function useReadingProgress() {
       // see how much total scroll is available
       const scrollHeight = document.body.scrollHeight - window.innerHeight;
       if (scrollHeight) {
-        setCompletion(
-          Number((currentProgress / scrollHeight).toFixed(2)) * 100
-        );
+        setCompletion(Number((currentProgress / scrollHeight).toFixed(2)) * 100);
       }
     }
-    // add scroll event listener
+
+    function handleRouteChange() {
+      setCompletion(0); // Reset completion when the route changes
+    }
+
+    // Add scroll and route change event listeners
     window.addEventListener("scroll", updateScrollCompletion);
- 
-    // remove scroll event listener on umount
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    // Remove event listeners on unmount
     return () => {
       window.removeEventListener("scroll", updateScrollCompletion);
+      router.events.off("routeChangeStart", handleRouteChange);
     };
-  }, []);
+  }, [router]);
+
   return completion;
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,6 +8,7 @@ import ThemeSwitch from "~/components/header/theme_switch";
 import { env } from "../../env.mjs";
 import { useReadingProgress } from "~/app_function/hooks/useReadingProgressbar";
 import { getPrefixRepo } from "~/app_function/utils/utils";
+import clsx from "clsx";
 
 const ConnectSection = dynamic(() => import("./connect_section"));
 const Links = dynamic(() => import("./links"));
@@ -24,6 +25,7 @@ export default function Header() {
   const [bgChange, setBgChange] = useState(false);
   const [loaded, setLoaded] = useState(false);
   const completion = useReadingProgress();
+
 
   useEffect(() => {
     setLoaded(true);
@@ -44,64 +46,63 @@ export default function Header() {
   }, []);
 
   return (
-    <header
-      className={`${loaded ? "transition-all delay-300 ease-in-out" : ""} ${
-        bgChange
-          ? "bg-base-200/70 shadow-2xl lg:xl:translate-y-1 lg:xl:rounded-xl lg:xl:ring-base-300/40 lg:xl:ring-1 lg:xl:mx-1"
-          : "shadow-none"
-      } supports-backdrop-blur:bg-white/60 sticky top-0 z-50 h-14 backdrop-blur-sm`}
-    >
-      <span
-        id="progress-bar"
+      <header
         style={{
-          transform: `translateX(${completion - 100}%)`,
+          backgroundSize: `${completion}%`,
+          backgroundRepeat: "no-repeat"
         }}
-        className={`absolute top-0 h-1 w-full bg-base-content/40 transition-transform duration-150 ease-in-out`}
-      />
-      <div className="container mx-auto flex h-14 items-center justify-between px-4 py-2">
-        <div className="flex items-center gap-2">
-          <span className="tooltip tooltip-bottom" data-tip="Go home">
-            <Link
-              href="/"
-              className="btn-ghost btn px-2 text-xl font-bold normal-case tracking-wider"
-            >
-              <Image
-                src={`${getPrefixRepo()}/images/logos/github-profile-dark${
-                  isLight ? "-light" : ""
-                }.png`}
-                width="32"
-                height="32"
-                alt={env.NEXT_PUBLIC_PERSON_NAME}
-              />
-              <div className="ml-2 hidden lg:flex">
-                {env.NEXT_PUBLIC_PERSON_NAME}
-              </div>
-            </Link>
-          </span>
-          <Links className="hidden items-center gap-4 md:flex lg:flex xl:flex" />
-        </div>
-        <div>
-          <div className="hidden lg:inline ">
-            <ConnectSection />
-          </div>
-          <div className="flex items-center justify-end lg:hidden">
-            <SearchButton />
-            <ThemeSwitch />
-            <div className="dropdown-end dropdown">
-              <label tabIndex={0} className="btn-ghost btn-circle btn">
-                <Bars3Icon className="h-8 w-8" />
-              </label>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu rounded-box mt-2 w-fit bg-base-100/95 px-2 py-5 shadow-2xl ring ring-base-300"
+        className={clsx(
+          loaded ? "transition-all duration-500 ease-in-out" : "",
+          bgChange
+            ? "bg-gradient-to-r from-30% from-base-300/30 to-base-content/50 to-100% shadow-2xl lg:mx-1 lg:translate-y-1 lg:rounded-xl lg:ring-1 lg:ring-base-300/40"
+            : "shadow-none",
+          "supports-backdrop-blur:bg-white/60 sticky top-0 z-50 h-14 backdrop-blur-sm"
+        )}
+      >
+        <div className="container mx-auto flex h-14 items-center justify-between overflow-visible px-4 py-2">
+          <div className="flex items-center gap-2">
+            <span className="tooltip tooltip-bottom" data-tip="Go home">
+              <Link
+                href="/"
+                className="btn-ghost btn px-2 text-xl font-bold normal-case tracking-wider"
               >
-                <Links className=" flex justify-center space-x-4 rounded-lg bg-base-100/80 px-2 pb-2" />
-                <ConnectSection />
-              </ul>
+                <Image
+                  src={`${getPrefixRepo()}/images/logos/github-profile-dark${
+                    isLight ? "-light" : ""
+                  }.png`}
+                  width="32"
+                  height="32"
+                  alt={env.NEXT_PUBLIC_PERSON_NAME}
+                />
+                <div className="ml-2 hidden lg:flex">
+                  {env.NEXT_PUBLIC_PERSON_NAME}
+                </div>
+              </Link>
+            </span>
+            <Links className="hidden items-center gap-4 md:flex lg:flex xl:flex" />
+          </div>
+          <div>
+            <div className="hidden lg:inline ">
+              <ConnectSection />
+            </div>
+            <div className="flex items-center justify-end lg:hidden">
+              <SearchButton />
+              <ThemeSwitch />
+              <div className="dropdown-end dropdown">
+                <label tabIndex={0} className="btn-ghost btn-circle btn">
+                  <Bars3Icon className="h-8 w-8" />
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="dropdown-content menu rounded-box mt-2 w-fit bg-base-100/95 px-2 py-5 shadow-2xl ring ring-base-300"
+                >
+                  <Links className=" flex justify-center space-x-4 rounded-lg bg-base-100/80 px-2 pb-2" />
+                  <ConnectSection />
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
   );
 }
