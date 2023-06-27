d03965a73d42751ae282b3613289695b0df06a6a
Biplob Sutradhar <biplobsd11@gmail.com>
2023-06-27 21:30:01 +0600

    code syntax highlighter

--- src/components/markdown/md_render.tsx
+++ src/components/markdown/md_render.tsx
@@ -1,24 +1,40 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
 import dynamic from "next/dynamic";
-import {
-  // type PluggableList,
-  type ReactMarkdownOptions
-} from "react-markdown/lib/react-markdown";
-// import { useEffect, useState } from "react";
+import { type ReactMarkdownOptions } from "react-markdown/lib/react-markdown";
 import AbsoluteLoading from "./absolute_loading";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
 
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import React, { useState } from "react";
+import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { delay } from "~/components/sapage/src/components/app/helper";
+import DocumentDuplicateIcon from "@heroicons/react/24/outline/DocumentDuplicateIcon";
+
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
+import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
+import markdown from "react-syntax-highlighter/dist/cjs/languages/prism/markdown";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+import lua from "react-syntax-highlighter/dist/cjs/languages/prism/lua";
+
+SyntaxHighlighter.registerLanguage("tsx", tsx);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("scss", scss);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("markdown", markdown);
+SyntaxHighlighter.registerLanguage("json", json);
+SyntaxHighlighter.registerLanguage("lua", lua);
+
 const ReactMarkdown = dynamic<ReactMarkdownOptions>(
   () =>
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-member-access
     import("react-markdown").then((e: any) => e.default),
   {
-    loading: () => <AbsoluteLoading />
+    loading: () => <AbsoluteLoading />,
   }
 );
 
@@ -27,44 +43,83 @@ interface MDRender {
 }
 
 export default function MDRender(props: MDRender) {
-  // const [rehypeRaw, setRehypeRaw] = useState<PluggableList>();
-  // const [remarkGfm, setRemarkGfm] = useState<PluggableList>();
-
-  // useEffect(() => {
-  //   const t = async () => {
-  //     const m = (await import("rehype-raw")).default;
-  //     setRehypeRaw([m]);
-  //   };
-  //   void t();
-
-  //   const t1 = async () => {
-  //     const m = (await import("remark-gfm")).default;
-  //     setRemarkGfm([m]);
-  //   };
-
-  //   void t1();
-  //   void t();
-  // }, []);
-
   return (
-    <article
-      className="container prose prose-stone relative mx-auto max-w-3xl px-2 prose-headings:my-1 prose-a:my-1 prose-a:text-blue-600 prose-a:no-underline prose-li:my-0 prose-img:my-2 prose-img:inline-block prose-img:rounded-xl">
-      {/* {(!rehypeRaw || !remarkGfm) && <AbsoluteLoading />} */}
+    <article className="container prose prose-stone relative mx-auto max-w-3xl px-2 prose-headings:my-1 prose-a:my-1 prose-a:text-blue-600 prose-a:no-underline prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0 prose-li:my-0 prose-img:my-2 prose-img:inline-block prose-img:rounded-xl">
       <ReactMarkdown
         components={{
           a: ({ children, ...props }) => {
             if (props.href?.includes("http")) {
               props.target = "_blank";
               props.rel = "noopener noreferrer";
-              props.className = "hover:underline";
+              props.className = "hover:underline break-all";
             }
             return <a {...props}>{children}</a>;
-          }
-          // img: ({ ...props }) => (
-          //   <span className="flex justify-center">
-          //     <img {...props} />
-          //   </span>
-          // ),
+          },
+          code({ inline, className, ...props }) {
+            const hasLang = /language-(\w+)/.exec(className || "");
+            return !inline && hasLang ? (
+              <SyntaxHighlighter
+                style={oneDark}
+                language={hasLang[1]}
+                PreTag="div"
+                className="scrollbar-style mockup-code"
+                showLineNumbers={true}
+                useInlineStyles={true}
+              >
+                {String(props.children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props} />
+            );
+          },
+          pre: (pre) => {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            const codeChunk = (pre as any).node.children[0].children[0]
+              .value as string;
+
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            const [copyTip, setCopyTip] = useState("Copy code");
+
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+            const language = (pre as any).children[0]?.props.className.replace(
+              /language-/g,
+              ""
+            ) as string;
+
+            return (
+              <div className="relative overflow-x-hidden">
+                <button
+                  style={{
+                    right: 0,
+                  }}
+                  className="tooltip tooltip-left absolute z-40 mr-2 mt-5"
+                  data-tip={copyTip}
+                >
+                  <CopyToClipboard
+                    text={codeChunk}
+                    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                    onCopy={async () => {
+                      setCopyTip("Copied");
+                      await delay(500);
+                      setCopyTip(`Copy code`);
+                    }}
+                  >
+                    <DocumentDuplicateIcon className="h-5 w-5 cursor-pointer hover:text-blue-600" />
+                  </CopyToClipboard>
+                </button>
+                <span
+                  style={{
+                    bottom: 0,
+                    right: 0,
+                  }}
+                  className="absolute z-40 mb-5 mr-1 rounded-lg bg-base-content/40 p-1 text-xs uppercase text-base-300 backdrop-blur-sm"
+                >
+                  {language}
+                </span>
+                <pre {...pre}></pre>
+              </div>
+            );
+          },
         }}
         rehypePlugins={[rehypeRaw]}
         remarkPlugins={[remarkGfm]}
