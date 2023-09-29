39f9efb2a3c51a8ca5c6cbcea4b9be38865dd759
Biplob Sutradhar <biplobsd11@gmail.com>
2023-09-29 11:43:24 +0600

    dropdown menu

--- src/components/header/hamburger.tsx
+++ src/components/header/hamburger.tsx
@@ -1,39 +1,41 @@
 import dynamic from "next/dynamic";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import { X } from "lucide-react";
-import { useRef } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { useEffect, useState } from "react";
 
 const ConnectSection = dynamic(() => import("./connect_section"));
 const Links = dynamic(() => import("./links"));
 
 export default function Hamburger() {
-  const ref = useRef<HTMLDetailsElement>(null);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDropdownOpen(false);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <>
-      <details ref={ref} className="group dropdown dropdown-end focus:ring">
-        <summary
-          onPointerEnter={() => {
-            ref.current!.open = true;
-          }}
-          className="btn btn-circle btn-ghost swap swap-rotate group-open:swap-active"
-        >
-          <Bars3Icon className="swap-off h-8 w-8" />
-          <X className="swap-on h-8 w-8" />
-        </summary>
-        <ul
-          onBlur={() => {
-            ref.current!.open = false;
-          }}
-          onPointerLeave={() => {
-            ref.current!.open = false;
-          }}
-          className="menu dropdown-content rounded-box mt-2 w-fit bg-base-100/95 px-2 py-5 shadow-2xl ring ring-base-300"
-        >
-          <Links className="flex justify-center space-x-4 rounded-lg bg-base-100/80 px-2 pb-2" />
-          <ConnectSection />
-        </ul>
-      </details>
-    </>
+    <DropdownMenu open={isDropdownOpen} onOpenChange={setDropdownOpen}>
+      <DropdownMenuTrigger className="swap-rotate btn btn-circle btn-ghost swap data-[state=open]:swap-active">
+        <Bars3Icon className="swap-off h-8 w-8" />
+        <X className="swap-on h-8 w-8" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="menu dropdown-content rounded-box m-2 mt-2 w-fit overflow-visible border-none bg-base-100/95 px-2 py-5 text-base-content shadow-2xl ring ring-base-300">
+        <Links className="flex justify-center space-x-4 rounded-lg bg-base-100/80 px-2 pb-2" />
+        <ConnectSection />
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
