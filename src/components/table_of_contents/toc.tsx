770a76f6037347ff538a26fc5dd0af1316588799
Biplob Sutradhar <biplobsd11@gmail.com>
2023-12-29 11:31:26 +0600

    Update Table of Contents styling

--- src/components/table_of_contents/toc.tsx
+++ src/components/table_of_contents/toc.tsx
@@ -6,8 +6,8 @@ export default function TableOfContents({ nodes }: RenderNodesProps) {
   }
 
   return (
-    <div className="max-w-xs rounded-2xl bg-base-100/80 p-2 ring-1 ring-base-content/5">
-      <h3 className="text-md">Table of contents</h3>
+    <div className="max-w-xs rounded-2xl bg-base-100/40 p-2 ring-1 ring-base-content/5">
+      <h3 className="text-md ml-2 mt-1 font-semibold">Table of contents</h3>
       <div className="menu menu-xs">
         <RenderNodes nodes={nodes} />
       </div>
