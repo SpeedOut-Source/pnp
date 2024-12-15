1813f2c8e17a7c4626ee74e269979a794464df07
Biplob Kumar Sutradhar <biplob.caretutors@gmail.com>
2024-12-15 13:10:29 +0600

    Added all tags page

--- src/components/me_section/r_x_t.tsx
+++ src/components/me_section/r_x_t.tsx
@@ -10,55 +10,81 @@ import Link from "next/link";
 
 import { WrenchScrewdriverIcon } from "@heroicons/react/24/outline";
 import { type TotalTagsType } from "~/app_function/utils/utils-server";
+import { cn } from "~/lib/utils";
 
-export default function RXT({ tags }: TotalTagsType) {
+interface Props extends TotalTagsType {
+  className?: string;
+  title?: string;
+  classNameMenubar?: string;
+}
+
+export default function RXT({
+  tags,
+  className,
+  classNameMenubar,
+  title = "Recent Expertise Technology",
+}: Props) {
   return (
-    <div className="p-card h-fit w-full space-y-4 p-4 text-justify xl:max-w-md">
+    <div
+      className={cn(
+        "p-card h-fit w-full space-y-4 p-4 text-justify xl:max-w-md",
+        className,
+      )}
+    >
       <p className="flex w-full items-center gap-2 text-start font-bold">
         <WrenchScrewdriverIcon className="h-5 w-5" />
-        Recent Expertise Technology
+        <Link href="/tags" className="cursor-pointer hover:underline">
+          {title}
+        </Link>
       </p>
-        <Menubar className="flex flex-wrap !border-none !bg-transparent !p-0 !h-auto justify-evenly">
-          {tags.map(({ tag, exist, total }) => (
-            <MenubarMenu key={tag}>
-              <MenubarTrigger className="capitalize hover:underline !cursor-pointer !py-0.5 !px-0 focus:!bg-transparent focus:!text-base-content data-[state=open]:!bg-transparent data-[state=open]:text-base-content">
-                {tag}{" "}
-                {total > 1 ? (
-                  <span className="indicator-new ml-1 bg-base-100">
-                    <span>{total}</span>
-                  </span>
-                ) : (
-                  <></>
-                )}
-                <span>{tag === tags[tags.length - 1]!.tag ? "" : ","}</span>
-              </MenubarTrigger>
+      <Menubar
+        className={cn(
+          "flex !h-auto flex-wrap justify-evenly !border-none !bg-transparent !p-0",
+          classNameMenubar,
+        )}
+      >
+        {tags.map(({ tag, exist, total }) => (
+          <MenubarMenu key={tag}>
+            <MenubarTrigger className="!cursor-pointer !px-0 !py-0.5 capitalize data-[state=open]:!bg-transparent data-[state=open]:text-base-content hover:underline focus:!bg-transparent focus:!text-base-content">
+              {tag}{" "}
+              {total > 1 ? (
+                <span className="indicator-new ml-1 bg-base-100">
+                  <span>{total}</span>
+                </span>
+              ) : (
+                <></>
+              )}
+              <span>{tag === tags[tags.length - 1]!.tag ? "" : ","}</span>
+            </MenubarTrigger>
 
-              <MenubarContent className="!border-base-300 !bg-base-100 !text-base-content">
-                <MenubarItem disabled className="capitalize !text-base-content">
-                  tag mentioned in
-                </MenubarItem>
-                <MenubarSeparator className="!bg-base-content/30 " />
-                {exist.map(({ type, total: itemTotal }) => (
-                  <Link
-                    key={`/${type}/page/tag/${tag}`}
-                    href={`/${type}/page/tag/${tag}`}
+            <MenubarContent
+              sideOffset={0}
+              className="!border-base-300 !bg-base-100 !text-base-content"
+            >
+              <MenubarItem disabled className="capitalize !text-base-content">
+                tag mentioned in
+              </MenubarItem>
+              <MenubarSeparator className="!bg-base-content/30" />
+              {exist.map(({ type, total: itemTotal }) => (
+                <Link
+                  key={`/${type}/page/tag/${tag}`}
+                  href={`/${type}/page/tag/${tag}`}
+                >
+                  <MenubarItem
+                    key={type}
+                    className="flex !cursor-pointer justify-between capitalize focus:!bg-base-300 focus:!text-base-content"
                   >
-                    <MenubarItem
-                      key={type}
-                      className="flex justify-between capitalize focus:!bg-base-300 focus:!text-base-content !cursor-pointer"
-                    >
-                      <span>{type}</span>
-                      <span className="indicator-new ml-1">
-                        <span>{itemTotal}</span>
-                      </span>
-                    </MenubarItem>
-                  </Link>
-                ))}
-              </MenubarContent>
-            </MenubarMenu>
-          ))}
-        </Menubar>
-
+                    <span>{type}</span>
+                    <span className="indicator-new ml-1">
+                      <span>{itemTotal}</span>
+                    </span>
+                  </MenubarItem>
+                </Link>
+              ))}
+            </MenubarContent>
+          </MenubarMenu>
+        ))}
+      </Menubar>
     </div>
   );
 }
