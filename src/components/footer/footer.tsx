7ebbc4f6b9c34fe72e5232a3ef038328308b1131
Biplob Sutradhar <biplobsd11@gmail.com>
2024-12-25 19:33:06 +0600

    Fixed small screen footer title align

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
-import { env } from "../../env.mjs";
 import GradientEffectBottom from "../gradient_effect/bottom";
+import { env } from "~/env.mjs";
 
 const Link = dynamic(() => import("next/link"));
 
@@ -12,10 +12,11 @@ export default function Footer() {
           <div className="container">
             <div className="divider" />
             <div className="mb-4 mt-2 flex flex-col items-center justify-between space-y-2 sm:flex-row sm:space-y-0">
-              <span className="text-sm">
-                {" "}
-                © 2025 {env.NEXT_PUBLIC_PERSON_NAME} | Made in Bangladesh
-              </span>
+              <div className="flex flex-col sm:flex-row items-center justify-center gap-2 sm:gap-0">
+                <span className="text-sm">© 2025 {env.NEXT_PUBLIC_PERSON_NAME}</span>
+                <span className="hidden sm:block mx-2">|</span>
+                <span className="text-sm">Made in Bangladesh</span>
+              </div>
               <ul className="flex flex-wrap space-x-4 text-sm">
                 <li>
                   <Link href="/about" className="link-hover link">
